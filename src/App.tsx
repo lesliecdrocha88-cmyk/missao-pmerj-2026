2ca07558a65e6395a9f7763c2ada3e2ce8bfc6d2
lesliecdrocha88-cmyk <lesliecd.rocha88@gmail.com>
2026-06-20 12:36:16 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ export default function App() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  // Página de obrigado
   if (window.location.pathname === "/obrigado") {
     return <Obrigado />;
   }
